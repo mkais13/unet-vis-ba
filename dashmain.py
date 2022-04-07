78382f4e9b23be69c65b7c77fa9cfc5dd8d0f0ca
mkais13 <m_kais13@uni-muenster.de>
2022-04-07 13:49:58 +0200

    dropdown table is now interactive

--- dashmain.py
+++ dashmain.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 import numpy as np
+import dash
 
 from dash import Dash, dcc, html, Input, Output, State, dash_table, callback_context
 import os
@@ -103,15 +104,27 @@ dbc.Container([
                                 html.Div(id="opt_text" , children=["Optimizer"]),
                                 html.Div(id="tf_text" , children=["Topologyfactor"]),
                                 html.Div(id="ki_text" , children=["Kernelinitialitzer"]),
-
+                            ], style={"justify" : "space-between"}),
+                            dbc.Col([
+                                html.Div(id="value_header" , children=["Value"]),
+                                dcc.Dropdown(id ="bs_dropdown"),
+                                dcc.Dropdown(id ="lf_dropdown"),
+                                dcc.Dropdown(id ="opt_dropdown"),
+                                dcc.Dropdown(id ="tf_dropdown"),
+                                dcc.Dropdown(id ="ki_dropdown"),
                             ]),
                             dbc.Col([
-                                 html.Div(id="value_header" , children=["Value"]),
-                                 dcc.Dropdown(id ="bs_dropdown",options=create_picture_options(),value="0"),
-                                 dcc.Dropdown(id ="lf_dropdown",options=create_picture_options(),value="0"),
-                                 dcc.Dropdown(id ="opt_dropdown",options=create_picture_options(),value="0"),
-                                 dcc.Dropdown(id ="tf_dropdown",options=create_picture_options(),value="0"),
-                                 dcc.Dropdown(id ="ki_dropdown",options=create_picture_options(),value="0"),
+                                html.Div(id="checklist_header" , children=["Choose"]),
+                                dbc.Checklist(
+                                    options=[
+                                        {"value": "bs"},
+                                        {"value": "lf"},
+                                        {"value": "opt"},
+                                        {"value": "tf"},
+                                        {"value": "ki"},
+                                    ],
+                                    id="checklist_input",
+                                ),
                             ]),
                         ]),
                     ])
@@ -136,7 +149,8 @@ dbc.Container([
     ], style={} ),
     dcc.Store(id="current_dataframe"),
     dcc.Store(id="selected_runs"),
-    dcc.Store(id="not_selected_runs")
+    dcc.Store(id="not_selected_runs"),
+    dcc.Store(id="change-triggered-by-checkboxes")
 ], style={ "padding" : "2vh", "min-width" : "inherit"})
 
 ], style = {"margin" : 0, "padding-right" : 0, "padding-left" : 0, "min-width" : "100%"})
@@ -149,6 +163,7 @@ State(component_id="current_dataframe", component_property="data")],
 )
 
 def update_predicted_picture(selectedData, pic_id, data):
+    print("callback 'update_predicted_picture' triggered")
     if selectedData != None:
         img_path = ""
         run_id = ""
@@ -188,13 +203,20 @@ def update_original_picture(pic_id):
     Output(component_id="lf_dropdown", component_property="value"),
     Output(component_id="opt_dropdown", component_property="value"),
     Output(component_id="tf_dropdown", component_property="value"),
-    Output(component_id="ki_dropdown", component_property="value")
+    Output(component_id="ki_dropdown", component_property="value"),
+    Output(component_id="checklist_input", component_property="value")
 ],[
     Input(component_id="selected_runs", component_property="data"),
     State(component_id="not_selected_runs", component_property="data"),
+    State(component_id="change-triggered-by-checkboxes", component_property="data")
 ], prevent_initial_call=True)
 
-def update_table(selected_runs_json, not_selected_runs_json):
+def update_table(selected_runs_json, not_selected_runs_json, triggered_by_checkboxes):
+    if(triggered_by_checkboxes):
+        print("callback 'update_table' triggered, BUT by checkboxes, aborting")
+        return dash.no_update,dash.no_update,dash.no_update,dash.no_update,dash.no_update,dash.no_update
+
+    print("callback 'update_table' triggered by a change in 'selected_runs'")
     triggering_component = callback_context.triggered[0]['prop_id'].split('.')[0]
 
     #extract all unique hyperparameter options
@@ -210,9 +232,10 @@ def update_table(selected_runs_json, not_selected_runs_json):
             lf_value = selected_parameters[1][2:]
             opt_value = selected_parameters[2][3:]
             tf_value = selected_parameters[3][2:]
+            print("tf_value in update_table:", tf_value)
             ki_value = selected_parameters[4][2:]
 
-    return bs_value, lf_value, opt_value, tf_value, ki_value 
+    return bs_value, lf_value, opt_value, tf_value, ki_value, []
 
 #initializes the options for the hyperparameter table
 @app.callback([
@@ -227,33 +250,52 @@ def update_table(selected_runs_json, not_selected_runs_json):
 )
 
 def update_table_options(data):
-
+    print("callback 'update_table_options' triggered by a change in 'current_dataframe'")
     current_df = pd.read_json(data, orient= "index")
+    #get all unique options of the different hyperparameters
     bs_options = current_df["batchsize"].unique()
     lf_options = current_df["lossfunction"].unique()
     opt_options = current_df["optimizer"].unique()
-    tf_options = current_df["topologyfactor"].unique()
+    tf_options = current_df["topologyfactor"].unique().tolist()
+    for i in range(len(tf_options)):
+        tf_options[i] = str(tf_options[i])
+    # turn all tf-options into a strings, because of errors when passing floats to the dcc.Dropdown
     ki_options = current_df["kernelinitializer"].unique()
-
     return bs_options,lf_options,opt_options,tf_options,ki_options
 
 
 
+#updates the current dataframe
+@app.callback([
+    Output(component_id="current_dataframe", component_property="data")
+],[
+    Input(component_id="selected_picture_id", component_property="value"),
+    Input(component_id="selected_dimension", component_property="value"),
+])
+
+def update_current_dataframe(selected_picture_id, selected_dimension):
+    #simple read and return
+    data = pd.read_json("assets/data/embeddata/{0}/{1}.json".format(selected_dimension.lower(),selected_picture_id), orient="index")
+    return [data.to_json(orient = "index")]
+
+
+
 #updates similaritygraph 
-@app.callback(
-    [Output(component_id="similaritygraph", component_property="figure"),
-    Output(component_id="current_dataframe", component_property="data")],
-    [Input(component_id="selected_picture_id", component_property="value"),
+@app.callback([
+    Output(component_id="similaritygraph", component_property="figure"),
+    ],[
+    Input(component_id="selected_picture_id", component_property="value"),
     Input(component_id="selected_dimension", component_property="value"),
     Input(component_id="selected_runs", component_property="data"),
     Input(component_id="not_selected_runs", component_property="data"),
-    State(component_id="similaritygraph", component_property="figure")]
-)
+    State(component_id="similaritygraph", component_property="figure")
+])
 
 def update_graph(slctd_pic_id, slctd_dim, selected_runs_json, not_selected_runs_json, similarity_fig):
 
     data = pd.read_json("assets/data/embeddata/{0}/{1}.json".format(slctd_dim.lower(),slctd_pic_id), orient="index")
     triggering_component = callback_context.triggered[0]['prop_id'].split('.')[0]
+    print("callback 'update_graph' triggered by {}".format(triggering_component if triggering_component != "" else "initial callback"))
 
     #change similaritygraph from 2d to 3d or vice versa
     if slctd_dim == "3D":
@@ -265,7 +307,7 @@ def update_graph(slctd_pic_id, slctd_dim, selected_runs_json, not_selected_runs_
 
     #if callback is called on page-load, change nothing
     if triggering_component == "":
-        return similarity_fig, data.to_json(orient = "index")
+        return [similarity_fig]
     else:
         #extract current run-ids
     
@@ -299,7 +341,7 @@ def update_graph(slctd_pic_id, slctd_dim, selected_runs_json, not_selected_runs_
         print("sim_fig found {0} not selected runs, input was {1} runs".format(not_selected_counter, len(not_selected_run_ids)))
 
 
-        return similarity_fig, data.to_json(orient = "index")
+        return [similarity_fig]
                     
 
 
@@ -317,6 +359,7 @@ def update_graph(slctd_pic_id, slctd_dim, selected_runs_json, not_selected_runs_
 )
 
 def update_extendedview(selected_runs_json, not_selected_runs_json, acc_figInput, loss_figInput):
+    print("callback 'update_extendedview' triggered by a change in 'selected_runs'")
     #if something has been clicked
     if selected_runs_json != None:
         #extract current run-ids
@@ -393,7 +436,8 @@ def update_extendedview(selected_runs_json, not_selected_runs_json, acc_figInput
 #updates the selected runs when one of the components, which control selected runs, changes
 @app.callback(
     [Output(component_id="selected_runs", component_property="data"),
-    Output(component_id="not_selected_runs", component_property="data")],
+    Output(component_id="not_selected_runs", component_property="data"),
+    Output(component_id="change-triggered-by-checkboxes", component_property="data")],
     [Input(component_id="similaritygraph",component_property="selectedData"),
     Input(component_id="accuracygraph",component_property="selectedData"),
     Input(component_id="lossgraph",component_property="selectedData"),
@@ -402,63 +446,134 @@ def update_extendedview(selected_runs_json, not_selected_runs_json, acc_figInput
     Input(component_id="opt_dropdown", component_property="value"),
     Input(component_id="tf_dropdown", component_property="value"),
     Input(component_id="ki_dropdown", component_property="value"),
+    Input(component_id="checklist_input", component_property="value"),
     State(component_id="current_dataframe", component_property="data")], prevent_initial_call=True
 )
 
-def update_selected_runs(sim_selectedData, acc_selectedData, loss_selectedData, bs_dropdown_value, lf_dropdown_value, opt_dropdown_value, tf_dropdown_value, ki_dropdown_value, data):
-
+def update_selected_runs(sim_selectedData, acc_selectedData, loss_selectedData, bs_dropdown_value, lf_dropdown_value, opt_dropdown_value, tf_dropdown_value, ki_dropdown_value, checklist_value, data):
+    #current dataframe
     current_df = pd.read_json(data, orient= "index")
-
+    #save component which triggered the callback
     triggering_component = callback_context.triggered[0]['prop_id'].split('.')[0]
+    #group dropdowns for readabitily
+    dropdowns = {"bs_dropdown", "lf_dropdown", "opt_dropdown","tf_dropdown", "ki_dropdown"}
+    dropdown_values = {bs_dropdown_value, lf_dropdown_value, opt_dropdown_value, tf_dropdown_value, ki_dropdown_value}
 
+
+
+    #callback triggered by similaritygraph
     if triggering_component == "similaritygraph":
-        print("callback 'update_selected_runs' triggered through sim_graph with {} runs".format(len(sim_selectedData["points"])))
+        print("callback 'update_selected_runs' triggered by sim_graph with {} runs".format(len(sim_selectedData["points"])))
         if(sim_selectedData != None):
             selected_x_coordinates = []
             selected_y_coordinates = []
+            #save only the coordinates from selectedData
             for i in range(len(sim_selectedData["points"])):
                 selected_x_coordinates.append(sim_selectedData["points"][i]["x"])
                 selected_y_coordinates.append(sim_selectedData["points"][i]["y"])
+            #find and save runs in current dataframe where the coordinates match
             selected_runs_df = current_df.loc[current_df["y"].isin(selected_y_coordinates) & current_df["x"].isin(selected_x_coordinates)]
+            #save all runs that have not been located above
             not_selected_runs_df = current_df.loc[~current_df["run_id"].isin(selected_runs_df["run_id"])]
 
-            return selected_runs_df.to_json(orient = "index"), not_selected_runs_df.to_json(orient = "index")
+            return selected_runs_df.to_json(orient = "index"), not_selected_runs_df.to_json(orient = "index"), False
+
         else:
-            return None, None
-    
+            return None, None, False
+
+
+
+    #callback triggered by one of the metric-graphs
     elif triggering_component  == "accuracygraph" or triggering_component  == "lossgraph": 
         log_df = None
         selectedData = []
+
+        #load one of the datasets on the metrics to compare later
         if triggering_component  == "accuracygraph":
-            print("callback 'update_selected_runs' triggered through acc_graph with {} runs".format(len(acc_selectedData["points"])))
+            print("callback 'update_selected_runs' triggered by acc_graph with {} runs".format(len(acc_selectedData["points"])))
             log_df = pd.read_json("assets/data/losslogs/scalars/acc.json", orient="index")
             selectedData = acc_selectedData
 
         if triggering_component  == "lossgraph":
-            print("callback 'update_selected_runs' triggered through loss_graph with {} runs".format(len(loss_selectedData["points"])))
+            print("callback 'update_selected_runs' triggered by loss_graph with {} runs".format(len(loss_selectedData["points"])))
             log_df = pd.read_json("assets/data/losslogs/scalars/loss.json", orient="index")
             selectedData = loss_selectedData
+    
         selected_x_coordinates = []
         selected_y_coordinates = []
-        
-
+        #save only the coordinates from selectedData
         for i in range(len(selectedData["points"])):
             selected_x_coordinates.append(selectedData["points"][i]["x"])
             selected_y_coordinates.append(selectedData["points"][i]["y"])
-        
+        #find the runs that match the coordinates
         selected_runs_df_log_format = log_df.loc[log_df["value"].isin(selected_y_coordinates) & (log_df["step"]+1).isin(selected_x_coordinates)]
+        #save only the run-ids
         selected_run_ids = selected_runs_df_log_format["run"].tolist()
+        #find those run-ids in the current dataframe and save the complete run
         selected_runs_df = current_df.loc[current_df["run_id"].isin(selected_run_ids)]
+        #find all runs that have not been located above
         not_selected_runs_df = current_df.loc[~current_df["run_id"].isin(selected_run_ids)]
-        return selected_runs_df.to_json(orient = "index"), not_selected_runs_df.to_json(orient = "index")
+        return selected_runs_df.to_json(orient = "index"), not_selected_runs_df.to_json(orient = "index"), False
+    
+    #callback triggered by one of the dropdowns
+    elif(triggering_component in dropdowns and not checklist_value):
+        #in case of the dropdowns has no value selected
+        if(None in dropdown_values):
+            print("callback 'update_selected_runs' triggered by one of the dropdowns, BUT at least one value was 'None'")
+            return dash.no_update, dash.no_update
+        else:
+            print("callback 'update_selected_runs' triggered by one of the dropdowns")
+            selected_run_ids = []
+            tf_dropdown_value = float(tf_dropdown_value)
+            #stitch the run-id back together from the selected values in the dropdowns
+            selected_run_ids.append("bs" + str(bs_dropdown_value) + "-lf" + str(lf_dropdown_value) + "-opt" + str(opt_dropdown_value) + "-tf" + str(tf_dropdown_value) + "-ki" + str(ki_dropdown_value))
+            #find that run in the current dataframe
+            selected_runs_df = current_df.loc[current_df["run_id"].isin(selected_run_ids)]
+            #save all runs except the one found
+            not_selected_runs_df = current_df.loc[~current_df["run_id"].isin(selected_run_ids)]
+            return selected_runs_df.to_json(orient = "index"), not_selected_runs_df.to_json(orient = "index"), False
+    #callback triggered by one of the checkboxes or a change in one of the dropdowns
     else:
-        selected_run_ids = []
-        selected_run_ids.append("bs" + bs_dropdown_value + "lf" + lf_dropdown_value + "opt" + opt_dropdown_value + "tf" + tf_dropdown_value + "ki" + ki_dropdown_value)
-        selected_runs_df = current_df.loc[current_df["run_id"].isin(selected_run_ids)]
-        not_selected_runs_df = current_df.loc[~current_df["run_id"].isin(selected_run_ids)]
-        return selected_runs_df.to_json(orient = "index"), not_selected_runs_df.to_json(orient = "index")
-        
-
+        #(triggering_component == "checklist_input"):
+        print("callback 'update_selected_runs' triggered by one of the checkboxes or a change in one of the dropdowns")
+        #dict to match checkbox value to the corresponding dropdown
+        hp_switcher = {
+            "bs" : bs_dropdown_value,
+            "lf" : lf_dropdown_value,
+            "opt" : opt_dropdown_value,
+            "tf" : tf_dropdown_value,
+            "ki" : ki_dropdown_value
+        }
+        checkbox_without_corresponding_value = False
+        checked_values = {}
+        #loop through all selected checkboxes to see if one has None-value in its corresponding dropdown
+        for i in range(len(checklist_value)):
+            dropdown_value = hp_switcher.get(checklist_value[i])
+            if(dropdown_value == None):
+                print("{0}-checkbox checked although it's corresponding dropdown_value is 'None'".format(checklist_value[i]))
+                checkbox_without_corresponding_value = True
+            else:
+                #add {"hyperparameter" : "value"} to checked_values
+                cur_hyperparam = switch_hyperparameter(checklist_value[i])
+                if cur_hyperparam == "batchsize":
+                    dropdown_value = int(dropdown_value)
+                elif cur_hyperparam == "topologyfactor":
+                    dropdown_value = float(dropdown_value)
+                checked_values[cur_hyperparam] = dropdown_value
+        #dont change anything if a None-value is found
+        if(checkbox_without_corresponding_value):
+            return dash.no_update, dash.no_update, dash.no_update
+        else:
+            print("{} have been selected by the checkboxes".format(checked_values))
+            selected_runs_df = current_df
+            #find all rows in the dataframe where all conditions are met
+            for hyperparameter,value in checked_values.items():
+                selected_runs_df = selected_runs_df.loc[selected_runs_df[hyperparameter] == value]
+            #looks complicated, but essentially drops all selected rows in selected_runs_df out of current-df
+            not_selected_runs_df = current_df.merge(selected_runs_df, indicator=True, how="left")[lambda x: x._merge=='left_only'].drop('_merge',1)
+            return selected_runs_df.to_json(orient = "index"), not_selected_runs_df.to_json(orient = "index"), True
+            
+            
 
 def extract_current_ids(selected_runs_json, not_selected_runs_json):
     selected_runs_df = pd.read_json(selected_runs_json, orient="index")
@@ -466,6 +581,17 @@ def extract_current_ids(selected_runs_json, not_selected_runs_json):
     selected_run_ids = selected_runs_df["run_id"].tolist()
     not_selected_run_ids = not_selected_runs_df["run_id"].tolist()
     return selected_run_ids, not_selected_run_ids
+
+def switch_hyperparameter(shortform):
+    switcher = {
+        "bs" : "batchsize",
+        "lf" : "lossfunction",
+        "opt" : "optimizer",
+        "tf" : "topologyfactor",
+        "ki" : "kernelinitializer"
+    }
+    return switcher.get(shortform)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
